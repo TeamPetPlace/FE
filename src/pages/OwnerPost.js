e6b9f7b3b290668a14f92af77bea1ba3ce4b7d91
yunseo <lionloopy@naver.com>
2023-03-13 13:02:35 +0900

    Feat: 메인 Header/Layout/Footer 구조잡기 #4

--- src/pages/OwnerPost.js
+++ src/pages/OwnerPost.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Layout from "../components/common/Layout";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 function OwnerPost() {
-  return <div>OwnerPost</div>;
+  return (
+    <Layout>
+      <Header />
+      {/* <Footer /> */}
+    </Layout>
+  );
 }
 
 export default OwnerPost;
