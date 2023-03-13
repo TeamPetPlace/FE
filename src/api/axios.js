5c207ca28c723fad78b9799327ca729625750f06
JESin10 <shinej1029@gmail.com>
2023-03-13 15:40:35 +0900

    feat : 유저 및 사업자 회원가입 구조잡기 #2

---
+++ src/api/axios.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { getCookie, removeCookie, setCookie } from "./cookie";
+
+export const baseURL = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+// //인스턴스 request header
+instance.interceptors.request.use(
+  (config) => {
+    if (config.headers === undefined) return;
+    const access_token = getCookie("access_token");
+    config.headers["Authorization"] = `${access_token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const OriginRequestError = error.config;
+
+    if (error.response.status === 401 && !OriginRequestError._retry) {
+      OriginRequestError._retry = true;
+      const refresh_token = getCookie("refresh_token");
+
+      if (refresh_token) {
+        try {
+          console.log("재발급중...");
+          const response = await axios.get(
+            `${process.env.REACT_APP_SERVER_URL}/token`,
+            {
+              headers: {
+                Refresh_Token: `${refresh_token}`,
+              },
+            }
+          );
+          const access_token = response.headers.authorization;
+          setCookie("access_token", access_token);
+          OriginRequestError.header.Authorization = `${access_token}`;
+          console.log("재발급 완료, 재실행");
+          return baseURL(OriginRequestError);
+        } catch (error) {
+          //두가지 토큰 모두 만료시
+          removeCookie("access_token");
+          removeCookie("refresh_token");
+          alert("세션이 만료되었습니다. 다시 로그인해주세요!");
+          window.location.href("/");
+          return Promise.reject(error);
+        }
+      }
+    }
+    return Promise.reject("error", error);
+  }
+);
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import BusinessSignupForm from "../components/register/BusinessSignupForm";
+import UserSignupForm from "../components/register/UserSignupForm";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [loginmode, setLoginMode] = useState(0);
+
+  // function LoginMode() {
+  //   setLoginMode(false);
+  // }
+
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            setLoginMode(0);
+          }}
+        >
+          일반회원
+        </button>
+        <button
+          onClick={() => {
+            setLoginMode(1);
+          }}
+        >
+          사업자회원
+        </button>
+      </div>
+      <div>{loginmode === 0 ? <UserSignupForm /> : <BusinessSignupForm />}</div>
+    </>
+  );
 }
 
 export default Signup;
