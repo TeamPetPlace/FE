90ba294b4f92665686842a4db29c54eaa234f013
JESin10 <shinej1029@gmail.com>
2023-04-01 01:14:50 +0900

    style : 반응형 css #107

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -25,20 +25,53 @@ const StBanner = styled.div`
   height: 513px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  padding-left: 340px;
-  padding-right: 340px;
   background-image: url(${shop_banner});
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    height: 150px;
+    background-size: auto 150px;
+    background-repeat: no-repeat;
+    background-position: top right 50%;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    width: 100%;
+    height: 300px;
+    background-size: auto 300px;
+    background-repeat: no-repeat;
+    background-position: top right 40%;
+  }
 `;
 
 const StTitle = styled.div`
   font-size: 36px;
   font-weight: bold;
+  width: 300px;
+  z-index: 99;
+  margin: 200px 0 0 350px;
+  @media screen and (max-width: 767px) {
+    margin: 13% 0 0 8%;
+    font-size: 15px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    margin: 13% 0 0 10%;
+    font-size: 25px;
+  }
 `;
 
 const StContent = styled.div`
   font-size: 22px;
   color: #555555;
   font-weight: bold;
-  padding-top: 20px;
+  padding-top: 10px;
+  width: 300px;
+  margin: 0 0 0 350px;
+  @media screen and (max-width: 767px) {
+    margin: 0 0 0 8%;
+    font-size: 10px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    margin: 0 0 0 10%;
+    font-size: 15px;
+  }
 `;
