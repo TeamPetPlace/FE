92469bf13b09b94c586b298815fa96f2a73fa58a
yunseo <lionloopy@naver.com>
2023-04-10 20:46:21 +0900

    feat : 에러페이지 구현 완료

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
-
+import PrivateRoute from "../components/register/PrivateRoute";
 // import Login from "../pages/Login";
 // import Signup from "../pages/Signup";
 // import Mypage from "../pages/Mypage";
@@ -38,20 +38,100 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/notification" element={<Notification />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/hospital" element={<Hospital />} />
-          <Route path="/hospital/:id" element={<HospitalDetail />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:id" element={<ShopDetail />} />
-          <Route path="/cafe" element={<Cafe />} />
-          <Route path="/cafe/:id" element={<CafeDetail />} />
-          <Route path="/ownerpost" element={<OwnerPost />} />
           <Route path="/kakao/callback" element={<Redirect />} />
+          <Route
+            path="/mypage"
+            element={
+              <PrivateRoute>
+                <Mypage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/notification"
+            element={
+              <PrivateRoute>
+                <Notification />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/main"
+            element={
+              <PrivateRoute>
+                <Main />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/hospital"
+            element={
+              <PrivateRoute>
+                <Hospital />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/hospital/:id"
+            element={
+              <PrivateRoute>
+                <HospitalDetail />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/shop"
+            element={
+              <PrivateRoute>
+                <Shop />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/shop/:id"
+            element={
+              <PrivateRoute>
+                <ShopDetail />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/cafe"
+            element={
+              <PrivateRoute>
+                <Cafe />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/cafe/:id"
+            element={
+              <PrivateRoute>
+                <CafeDetail />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/ownerpost"
+            element={
+              <PrivateRoute>
+                <OwnerPost />
+              </PrivateRoute>
+            }
+          />
+          {/* <Route path="/mypage" element={<Mypage />} /> */}
+          {/* <Route path="/notification" element={<Notification />} /> */}
+          {/* <Route path="/main" element={<Main />} /> */}
+          {/* <Route path="/hospital" element={<Hospital />} /> */}
+          {/* <Route path="/hospital/:id" element={<HospitalDetail />} /> */}
+          {/* <Route path="/shop" element={<Shop />} /> */}
+          {/* <Route path="/shop/:id" element={<ShopDetail />} /> */}
+          {/* <Route path="/cafe" element={<Cafe />} /> */}
+          {/* <Route path="/cafe/:id" element={<CafeDetail />} /> */}
+          {/* <Route path="/ownerpost" element={<OwnerPost />} /> */}
+
           <Route path="/*" element={<NotFound />} />
           <Route path="/ownerpage" element={<OwnerPage />} />
-
           <Route path="/error" element={<ErrorPage />} />
         </Routes>
       </Suspense>
